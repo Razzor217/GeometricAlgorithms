c15e23f733d8b8ec430774ae8be73a34b9aa7202
Max Beddies <ubyjy@student.kit.edu>
2020-09-16 22:32:47 +0200

    initial commit
    - add main cmakelist
    - add test stubs

---
+++ source/test/SweepLineTest.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE Geometry::SweepLine
+
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+
+BOOST_AUTO_TEST_CASE(hasTopNeighbourTest)
+{
+    BOOST_CHECK_EQUAL(true, true);
+}
